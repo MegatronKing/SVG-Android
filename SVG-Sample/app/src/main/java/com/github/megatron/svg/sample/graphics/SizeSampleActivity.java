d6e231f4a4d0841625bc98f5d3b6bb4956788db4
jgy08954 <jgy08954@ly.com>
2016-10-18 10:40:55 +0800

    update sample

--- SVG-Sample/app/src/main/java/com/github/megatron/svg/sample/graphics/SizeSampleActivity.java
+++ SVG-Sample/app/src/main/java/com/github/megatron/svg/sample/graphics/SizeSampleActivity.java
@@ -3,6 +3,10 @@ package com.github.megatron.svg.sample.graphics;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.github.megatron.svg.sample.R;
 
@@ -13,5 +17,18 @@ public class SizeSampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphics_size_sample);
         setTitle(getIntent().getStringExtra("title"));
+
+        Toast.makeText(this, "click to scale image!", Toast.LENGTH_LONG).show();
+
+        final ImageView imageView = (ImageView) findViewById(R.id.image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ViewGroup.LayoutParams lp = imageView.getLayoutParams();
+                lp.width = imageView.getMeasuredWidth() + 100;
+                lp.height = imageView.getMeasuredHeight() + 100;
+                imageView.setLayoutParams(lp);
+            }
+        });
     }
 }
