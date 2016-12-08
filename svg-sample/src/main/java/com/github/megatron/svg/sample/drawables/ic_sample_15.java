b0dd30938f4c15fa9ff0402b513429b1372279e7
jgy08954 <jgy08954@ly.com>
2016-12-08 15:38:52 +0800

    fix a bug in sample

--- svg-sample/src/main/java/com/github/megatron/svg/sample/drawables/ic_sample_15.java
+++ svg-sample/src/main/java/com/github/megatron/svg/sample/drawables/ic_sample_15.java
@@ -28,6 +28,58 @@ public class ic_sample_15 extends SVGRenderer {
         final float scaleX = w / 24.0f;
         final float scaleY = h / 24.0f;
         
+        mPath.reset();
+        mRenderPath.reset();
+        
+        mFinalPathMatrix.setValues(new float[]{1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f});
+        mFinalPathMatrix.postScale(scaleX, scaleY);
+        
+        mPath.moveTo(15.0f, 3.0f);
+        mPath.rLineTo(2.3f, 2.3f);
+        mPath.rLineTo(-2.89f, 2.87f);
+        mPath.rLineTo(1.42f, 1.42f);
+        mPath.lineTo(18.7f, 6.7f);
+        mPath.lineTo(21.0f, 9.0f);
+        mPath.lineTo(21.0f, 3.0f);
+        mPath.close();
+        mPath.moveTo(15.0f, 3.0f);
+        mPath.moveTo(3.0f, 9.0f);
+        mPath.rLineTo(2.3f, -2.3f);
+        mPath.rLineTo(2.87f, 2.89f);
+        mPath.rLineTo(1.42f, -1.42f);
+        mPath.lineTo(6.7f, 5.3f);
+        mPath.lineTo(9.0f, 3.0f);
+        mPath.lineTo(3.0f, 3.0f);
+        mPath.close();
+        mPath.moveTo(3.0f, 9.0f);
+        mPath.moveTo(9.0f, 21.0f);
+        mPath.rLineTo(-2.3f, -2.3f);
+        mPath.rLineTo(2.89f, -2.87f);
+        mPath.rLineTo(-1.42f, -1.42f);
+        mPath.lineTo(5.3f, 17.3f);
+        mPath.lineTo(3.0f, 15.0f);
+        mPath.rLineTo(0f, 6.0f);
+        mPath.close();
+        mPath.moveTo(9.0f, 21.0f);
+        mPath.moveTo(21.0f, 15.0f);
+        mPath.rLineTo(-2.3f, 2.3f);
+        mPath.rLineTo(-2.87f, -2.89f);
+        mPath.rLineTo(-1.42f, 1.42f);
+        mPath.rLineTo(2.89f, 2.87f);
+        mPath.lineTo(15.0f, 21.0f);
+        mPath.rLineTo(6.0f, 0f);
+        mPath.close();
+        mPath.moveTo(21.0f, 15.0f);
+        
+        mRenderPath.addPath(mPath, mFinalPathMatrix);
+        if (mFillPaint == null) {
+            mFillPaint = new Paint();
+            mFillPaint.setStyle(Paint.Style.FILL);
+            mFillPaint.setAntiAlias(true);
+        }
+        mFillPaint.setColor(applyAlpha(-16777216, 1.0f));
+        mFillPaint.setColorFilter(filter);
+        canvas.drawPath(mRenderPath, mFillPaint);
 
     }
 
