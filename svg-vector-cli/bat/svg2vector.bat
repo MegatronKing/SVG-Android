bdc1fd9f26d5d25c93a1bc3ca9d97a847aa5fe2a
jgy08954 <jgy08954@ly.com>
2017-01-09 15:28:06 +0800

    add module cli and applet

--- svg-vector-cli/bat/svg2vector.bat
+++ svg-vector-cli/bat/svg2vector.bat
@@ -1,10 +1,1 @@
-@ ECHO OFF
-
-if  "%JAVA_HOME%" == "" (goto setJava) else (goto convert)
-:setJava
-@ ECHO 请安装Java JDK并设置环境变量
-goto end
-
-:convert
 java -jar svg2vector-cli.jar -d %cd% -o %cd%
-:end
