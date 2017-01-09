0ee13b7598b1e1a69f34b398830715ea2361d510
jgy08954 <jgy08954@ly.com>
2017-01-09 15:49:58 +0800

    add module cli and applet

--- svg-vector-applet/src/main/java/com/github/megatronking/svg/applet/ui/OpenFilePanel.java
+++ svg-vector-applet/src/main/java/com/github/megatronking/svg/applet/ui/OpenFilePanel.java
@@ -2,6 +2,8 @@ package com.github.megatronking.svg.applet.ui;
 
 import com.github.megatronking.svg.applet.graphics.GraphicsUtilities;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -10,6 +12,8 @@ import java.net.URL;
 import javax.swing.JComponent;
 
 class OpenFilePanel extends JComponent {
+
+    private static final String DROP_TEXT = "Drop Here";
 
     private BufferedImage dropHere;
 
@@ -32,7 +36,10 @@ class OpenFilePanel extends JComponent {
     protected void paintComponent(Graphics g) {
         int x = (getWidth() - dropHere.getWidth()) / 2;
         int y = (getHeight() - dropHere.getHeight()) / 2;
-
+        g.setFont(new Font("宋体", Font.BOLD, 32));
+        g.setColor(Color.WHITE);
+        int textWidth = g.getFontMetrics().stringWidth(DROP_TEXT);
+        g.drawString(DROP_TEXT, (getWidth() - textWidth) / 2, y - 30);
         g.drawImage(dropHere, x, y, null);
     }
 
